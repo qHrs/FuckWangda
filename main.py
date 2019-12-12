e6e46c99504e7c3dfc914b5633792cedf3bc47b2
qpanda <qpanda@126.com>
2019-12-13 00:10:03 +0800

    修改暂停问题

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 #-------------------------------------------------------------------------------
 # Name:         main
-# Description:  
+# Description:  FuckWangda
 # Author:       qpand
 # Date:         2019/12/10
 #-------------------------------------------------------------------------------
@@ -21,12 +21,11 @@ options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属
 options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
 options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
 options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
-#options.add_argument('User-Agent=Mozilla/5.0 (Linux; U; Android 8.1.0; zh-cn; BLA-AL00 Build/HUAWEIBLA-AL00) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/8.9 Mobile Safari/537.36')
 browser = webdriver.Chrome(executable_path=executable_path, options=options)
 
-#打开页面
+# 打开登陆页面
 browser.get("https://wangda.andedu.net/")
-time.sleep(10)
+time.sleep(5)
 
 print(browser.title)
 
@@ -44,66 +43,88 @@ html = browser.page_source
 if html.find("退出账号"):
     print("登陆成功")
 
-    surl = "https://wangda.andedu.net/#/study/subject/detail/e8b602bb-8731-4b5b-8d4d-67c0f1b2eba7"  # 揭秘云计算
+    # 课程url
+    urls = ["https://wangda.andedu.net/#/study/subject/detail/a8dfcf08-a8fa-4963-9a6e-44448a540e28",
+            "https://wangda.andedu.net/#/study/subject/detail/e8b602bb-8731-4b5b-8d4d-67c0f1b2eba7",
+            "https://wangda.andedu.net/#/study/subject/detail/1a7a41e5-6b7c-4074-b2d7-7c85a857280e",
+            "https://wangda.andedu.net/#/study/subject/detail/46b0c273-0eb2-4cba-9117-af13f751813b",
+            "https://wangda.andedu.net/#/study/subject/detail/271080e4-0e39-4f7b-961c-cc0e5bffba2b",
+            "https://wangda.andedu.net/#/study/subject/detail/73bfb6cd-ad81-4b9e-ac5d-c8694f49eb2d",
+            "https://wangda.andedu.net/#/study/subject/detail/49060db8-fd8a-401e-aaf1-727281900af1",
+            "https://wangda.andedu.net/#/study/subject/detail/33b5fe33-79ba-47dc-bec0-3b7a5ce7b2ce",
+            "https://wangda.andedu.net/#/study/subject/detail/85400297-7e8b-4dab-88a4-7959cfa0fd10"]
 
-    browser.get(surl)
-    time.sleep(5)
-    html = browser.page_source
-    selector = etree.HTML(html)
-    classList = selector.xpath('//div[contains(@class,"catalog-state-info")]/div/div[2]/div[1]/text()')
-    classStatus = selector.xpath('//div[contains(@class,"catalog-state-info")]/div/div[3]/a/div/text()')
-    classIdList = selector.xpath('//div[contains(@class,"catalog-state-info")]/div/div[3]/a/@data-resource-id')
+    # 永远都学不完的bug
+    #bugUrl = ["40f280e7-09f1-43e4-b7da-5e1edd70f1e1"]
+    bugUrl = []
 
-    courseList = []
-    for title, status, classId in zip(classList, classStatus, classIdList):
-        if status in ["继续学习", "开始学习"]:
-            course = {}
-            course["name"] = title
-            course["status"] = status
-            course["classId"] = classId
-            courseList.append(course)
+    for url in urls:
+        browser.get(url)
+        time.sleep(5)
+        html = browser.page_source
+        selector = etree.HTML(html)
+        classList = selector.xpath('//div[contains(@class,"catalog-state-info")]/div/div[2]/div[1]/text()')
+        classStatus = selector.xpath('//div[contains(@class,"catalog-state-info")]/div/div[3]/a/div/text()')
+        classIdList = selector.xpath('//div[contains(@class,"catalog-state-info")]/div/div[3]/a/@data-resource-id')
 
-    for item in courseList:
-        print("%s - %s - %s" % (item["name"], item["status"], item["classId"]))
+        courseList = []
+        for title, status, classId in zip(classList, classStatus, classIdList):
+            if status in ["继续学习", "开始学习"]:
+                if classId not in bugUrl:
+                    course = {}
+                    course["name"] = title
+                    course["status"] = status
+                    course["classId"] = classId
+                    courseList.append(course)
 
-    for item in courseList:
-        print("%s 开始学习" % (item["name"]))
+        for item in courseList:
+            print("%s - %s - %s" % (item["name"], item["status"], item["classId"]))
 
-        # 播放
-        # 课程url 0-视频id 1-课程id
-        subjectID = surl.split("/")[7]
-        #browser.get("https://wangda.andedu.net/#/study/course/detail/subject-course/f62887c2-69f3-4e17-ac15-a5b186411ea5/6/73bfb6cd-ad81-4b9e-ac5d-c8694f49eb2d")
-        courseUrl = "https://wangda.andedu.net/#/study/course/detail/subject-course/{0}/6/{1}"
-        browser.get(courseUrl.format(item["classId"], subjectID))
-        # '//div[contains(@class,"chapter-list")]/ul/li/div/dl/dd/div/span[1]/text()'
+        for item in courseList:
+            print("%s 开始学习" % (item["name"]))
 
-        playS = ''
-        while True:
-            html = browser.page_source
-            selector = etree.HTML(html)
-            playNewS = selector.xpath('//div[contains(@class,"chapter-list")]/ul/li/div/dl/dd/div/span[1]/text()')
-            if len(playNewS) > 0:
+            # 播放
+            # 课程url 0-视频id 1-课程id
+            subjectID = url.split("/")[7]
+            courseUrl = "https://wangda.andedu.net/#/study/course/detail/subject-course/{0}/6/{1}"
+            browser.get(courseUrl.format(item["classId"], subjectID))
+            # 等待播放
+            time.sleep(8)
 
-                if playS == "":
-                    playS = playNewS[0]
-                    print(playS)
+            playS = ''
+            while True:
+                html = browser.page_source
 
-                if playS in ["重新学习", "学习中"] :
-                    print("%s 已完成" % (item["name"]))
+                # 暂停检查，已暂停则跳出循环
+                if html.find("vjs-paused") > 0:
+                    print("\n已暂停")
+                    #browser.refresh()
+                    #time.sleep(5)
                     break
 
-                if playS != playNewS[0]:
-                    playS = playNewS[0]
-                    print(playS)
-                else:
-                    print("-", end='')
+                # 检查学习进度及完成状态，完成则退出循环进入下一节
+                selector = etree.HTML(html)
+                playNewS = selector.xpath('//div[contains(@class,"chapter-list")]/ul/li/div/dl/dd/div/span[1]/text()')
+                if len(playNewS) > 0:
 
-                time.sleep(2)
+                    # 检查是否已完成
+                    if playNewS[0] == "重新学习":
+                        print("%s 已完成1" % (item["name"]))
+                        break
 
-    # https://wangda.andedu.net/#/study/course/detail/subject-course/f9a4a6c5-152d-4971-a420-e4416ffcf509/6/e8b602bb-8731-4b5b-8d4d-67c0f1b2eba7
+                    if playS == "":
+                        playS = playNewS[0]
+                        print("%s" % playS)
 
-    # print(html)   data-resource-id  $x('//div[contains(@class,"catalog-state-info")]/div/div[3]/a[contains(@data-resource-id)]')
-    #//*[@id="D177studyBtn-67d0102f-8164-43cc-8d7a-01b2b94e4084"]
-    # https://wangda.andedu.net/#/study/course/detail/subject-course/d42558f8-ba03-4d23-89ed-c6d8b3fe5fc7/6/e8b602bb-8731-4b5b-8d4d-67c0f1b2eba7
-    # $x('//div[contains(@class,"chapter-list")]/ul/li/div/dl/dd/div/span[contains(@class,"progress")]/text()')
+                    if playS != playNewS[0]:
+                        playS = playNewS[0]
+                        print(playS)
+                    else:
+                        print("-", end='')
+
+                    if playS == "重新学习" :
+                        print("%s 已完成2" % (item["name"]))
+                        break
+
+                    time.sleep(2)
 
