e9e9a4d1096033aad2028cb8de00e93820f17ea3
qpand <qpanda@126.com>
2019-12-13 09:39:16 +0800

    增加禁止日志输出

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from lxml import etree
 
 options = webdriver.ChromeOptions()
 
-executable_path = r"webdriver\chromedriver.exe"
+executable_path = r"E:\PycharmProjects\FuckWangda\webdriver\chromedriver.exe"
 
 options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
 options.add_argument('window-size=1920x1080')  # 指定浏览器分辨率
@@ -21,6 +21,7 @@ options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属
 options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
 options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
 options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+options.add_argument('log-level=3')  # 只输出失败信息
 browser = webdriver.Chrome(executable_path=executable_path, options=options)
 
 # 打开登陆页面
